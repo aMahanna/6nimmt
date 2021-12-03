c3b694ccfb15d65d77a622ce83df76df7c8d6a31
aMahanna <anthony.mahanna@gmail.com>
2021-12-03 15:37:03 -0500

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
-echo "${PWD}" > temp
+[[ -z "$1" ]] && adb_path="${PWD}" || adb_path=$(echo "$1" | sed 's:/*$::') # Default path to arangodb as pwd
+[[ -z "$2" ]] && ent_dir="enterprise" || ent_dir=$(echo "$2" | sed 's:/*$::') # Default enterprise dir to enterprise
 
-# [[ -z "$1" ]] && adb_path="${PWD}" || adb_path=$(echo "$1" | sed 's:/*$::') # Default path to arangodb as pwd
-# [[ -z "$2" ]] && ent_dir="enterprise" || ent_dir=$(echo "$2" | sed 's:/*$::') # Default enterprise dir to enterprise
-
-# echo "ArangoDB directory: $adb_path"
-# echo "Enterprise directory: $adb_path/$ent_dir"
+echo "ArangoDB directory: $adb_path"
+echo "Enterprise directory: $adb_path/$ent_dir"
 
 # cd "$adb_path"
-# echo "$adb_path" > temp
+community_diff=$(git diff --diff-filter=ACMR --name-only -- '*.cpp' '*.hpp' '*.cc' '*.c' '*.h')
+
+if [ -d "$adb_path/$ent_dir" ] # assume enterprise directory is within arangodb directory
+then
+   cd "$adb_path/$ent_dir"
+   enterprise_diff=$(git diff --diff-filter=ACMR --name-only -- '*.cpp' '*.hpp' '*.cc' '*.c' '*.h' | sed "s,^,$ent_dir/,")
+fi
+
+diff="$community_diff $enterprise_diff"
+if [[ -n "${str// /}" ]]; then
+    echo "docker run -it --rm -v "$adb_path":/usr/src/arangodb clang-format:latest $diff"
+    docker run -it --rm -v "$adb_path":/usr/src/arangodb clang-format:latest $diff
+fi
