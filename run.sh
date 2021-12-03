cddeb72906a4c441980c4c5e5325804dbad0c2df
aMahanna <anthony.mahanna@gmail.com>
2021-12-03 15:07:26 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
-ls > temp
+echo "${PWD}" > temp
 
+# [[ -z "$1" ]] && adb_path="${PWD}" || adb_path=$(echo "$1" | sed 's:/*$::') # Default path to arangodb as pwd
+# [[ -z "$2" ]] && ent_dir="enterprise" || ent_dir=$(echo "$2" | sed 's:/*$::') # Default enterprise dir to enterprise
+
+# echo "ArangoDB directory: $adb_path"
+# echo "Enterprise directory: $adb_path/$ent_dir"
+
+# cd "$adb_path"
+# echo "$adb_path" > temp
